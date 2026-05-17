f3d514a2dcf3f8839d146477deeb2b5a88daac73
Josh Chen <josh@joshchen.dev>
2026-05-17 21:32:48 +0900

    exercise 16 done

---
+++ controllers/authors.js
@@ -0,0 +1,19 @@
+const { Blog } = require('../models')
+const { sequelize } = require('../util/db')
+
+const router = require('express').Router()
+
+router.get('/', async (req, res) => {
+  const authors = await Blog.findAll({
+    attributes: [
+      'author',
+      [sequelize.fn('COUNT', sequelize.col('author')), 'articles'],
+      [sequelize.fn('SUM', sequelize.col('likes')), 'likes']
+    ],
+    group: 'author',
+    order: [['likes', 'DESC']]
+  })
+  res.json(authors)
+})
+
+module.exports = router
